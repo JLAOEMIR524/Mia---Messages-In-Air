321073c056bde4deaa234202903383de293914b7
Sonja Schorn <sschorn.mmt-b2024@fh-salzburg.ac.at>
2026-04-14 11:23:43 +0200

    Starting with cars and icons button

---
+++ src/components/Card.tsx
@@ -0,0 +1,35 @@
+interface CardProps {
+  image?: string;
+  title?: string;
+  description?: string;
+  buttonText?: string;
+  onButtonClick?: () => void;
+  className?: string;
+}
+
+export function Card({ 
+  image, 
+  title, 
+  description, 
+  buttonText, 
+  onButtonClick,
+  className = "" 
+}: CardProps) {
+  return (
+    <div className={`card ${className}`}>
+      {image && <img src={image} alt={title || "Card Image"} />}
+      
+      <div className="card__body">
+        {title && <h3>{title}</h3>}
+        
+        {description && <p>{description}</p>}
+        
+        {buttonText && (
+          <button className="button button--primary" onClick={onButtonClick}>
+            {buttonText}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,5 +1,28 @@
+import { Card } from "../components/Card";
+
 export function Gallery() {
   return (
-    <p>Gallery</p>
+    <main className="left">
+      <h2>Your Gallery 🖼️</h2>
+      <p>All the postcards you've sent and received</p>
+      <div className="button-flex gallery">
+        <button className="button button--image">
+          <span className="icon-span"></span>
+          All Postcards
+        </button>
+        <button className="button button--image">
+          <span className="icon-span"></span>
+          Received
+        </button>
+        <button className="button button--image">
+          <span className="icon-span"></span>
+          Send
+        </button>
+      </div>
+      <Card
+        image="https://placehold.co/400x250"
+        description="Nur eine kurze Info ohne Überschrift."
+      />
+    </main>
   );
 }
