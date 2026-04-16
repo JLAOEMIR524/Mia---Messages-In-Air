ef1d390073d2497526738e5c435539a4d0c98ec0
Sonja Schorn <sschorn.mmt-b2024@fh-salzburg.ac.at>
2026-04-16 09:11:26 +0200

    adding logic for background to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import { NavBarTop } from "../components/NavbarTop";
 
 export function Home() {
-  document.body.classList.add("background-heaven");
+  useEffect(() => {
+    document.body.classList.add("background-heaven");
+
+    return () => {
+      document.body.classList.remove("background-heaven");
+    };
+  }, []);
   return (
     <main className="heaven">
       <NavBarTop />
@@ -9,19 +16,19 @@ export function Home() {
         Mia <span>Messages in Air</span>
       </h1>
       <div>
-      <p>A postcard to a stranger.</p>
-      <p>No likes. No followers. No feed.</p>
-      <p>Just real, unexpected messages.</p>
+        <p>A postcard to a stranger.</p>
+        <p>No likes. No followers. No feed.</p>
+        <p>Just real, unexpected messages.</p>
       </div>
       <div className="button-flex">
-        <button className="button button--primary">
-          Start now
-        </button>
-        <button className="button button--secondary">
-          Find out more
-        </button>
+        <button className="button button--primary">Start now</button>
+        <button className="button button--secondary">Find out more</button>
       </div>
-      <img src="./icons/letter-blue.svg" alt="Icon Mail" className="email-icon"/>
+      <img
+        src="./icons/letter-blue.svg"
+        alt="Icon Mail"
+        className="email-icon"
+      />
     </main>
   );
 }
