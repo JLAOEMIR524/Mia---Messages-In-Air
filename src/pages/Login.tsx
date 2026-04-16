cf33b45e3908a57ff1ef925449c95a8c20dcf303
Sonja Schorn <sschorn.mmt-b2024@fh-salzburg.ac.at>
2026-04-16 13:20:03 +0200

    basic strukture site

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Login() {
+    useEffect(() => {
+      document.body.classList.add("background-heaven");
+  
+      return () => {
+        document.body.classList.remove("background-heaven");
+      };
+    }, []);
+  
+    const navigate = useNavigate();
   return (
-    <p>Login</p>
+     <main className="heaven">
+      <button onClick={() => navigate("/dashboard")} className="arrowBack">
+        <img src="./icons/arrow-back.svg" alt="Arrow Back Icon" />
+      </button>
+    </main>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Register() {
+    useEffect(() => {
+      document.body.classList.add("background-heaven");
+  
+      return () => {
+        document.body.classList.remove("background-heaven");
+      };
+    }, []);
+  
+    const navigate = useNavigate();
   return (
-    <p>Register</p>
+     <main className="heaven">
+      <button onClick={() => navigate("/dashboard")} className="arrowBack">
+        <img src="./icons/arrow-back.svg" alt="Arrow Back Icon" />
+      </button>
+    </main>
   );
 }
