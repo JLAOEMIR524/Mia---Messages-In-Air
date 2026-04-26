024afa447a24597880362389640e700114055d1a
Sonja Schorn <sschorn.mmt-b2024@fh-salzburg.ac.at>
2026-04-26 09:35:23 +0200

    changed icon for step back

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -15,8 +15,8 @@ export function Details() {
   
   return (
     <main className="heaven">
-      <button onClick={() => navigate("/dashboard")} className="arrowBack">
-        <img src="./icons/close.svg" alt="Arrow Back Icon" />
+      <button onClick={() => navigate(-1)} className="arrowBack">
+        <img src="./icons/arrow-back.svg" alt="Arrow Back Icon" />
       </button>
 
       <FeedbackCard
