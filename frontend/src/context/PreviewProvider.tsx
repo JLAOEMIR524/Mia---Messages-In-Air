4b0f953124e1cae4020e016f45368acf3a1a2e7a
Julian Obermeier <jobermeier.mmt-b2024@fh-salzburg.ac.at>
2026-06-05 17:36:24 +0200

    Fixed linter issues complaining about a setState in useEffect

--- frontend/src/context/PreviewProvider.tsx
+++ frontend/src/context/PreviewProvider.tsx
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { PreviewContext } from "./PreviewContext";
 import { useLocation } from "react-router-dom";
 
 export function PreviewProvider({ children }: { children: React.ReactNode }) {
-  const [previewOpen, setPreviewOpen] = useState(false);
-
   const location = useLocation();
 
-  useEffect(() => {
-    setPreviewOpen(false);
-  }, [location.pathname]);
+  const [openedAtPath, setOpenedAtPath] = useState<string | null>(null);
+
+  const previewOpen = openedAtPath === location.pathname;
+  const setPreviewOpen = useCallback(
+    (open: boolean) => {
+      setOpenedAtPath(open ? location.pathname : null);
+    },
+    [location.pathname],
+  );
 
   return (
     <PreviewContext.Provider value={{ previewOpen, setPreviewOpen }}>
